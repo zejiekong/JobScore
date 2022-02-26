1645fd7192bb8139042dbcd82ec17562ad46d6cf
ZE JIE <ze-jie-kong-@hotmail.com>
2022-02-26 15:28:14 +0800

    fixed image,added register.html

--- flask.py
+++ flask.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template,redirect,url_for,request
 
 app = Flask(__name__)
 
+
 @app.route("/",method=["GET","POST"])
 def login():
     if request.method == "POST":
@@ -10,6 +11,13 @@ def login():
         
     else:
         return render_template("login.html")
+
+@app.route("/registration",method=["GET","POST"])
+def registration():
+    if request.method == "POST":
+        #register user credentials
+    else:
+        return render_template("registration.html")
 
 @app.route("/home")
 
