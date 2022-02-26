7440426d6fc88ed96a450e3401e10ad4abdca8f5
ZE JIE <ze-jie-kong-@hotmail.com>
2022-02-26 16:46:39 +0800

    corrected img src url

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from flask import Flask,render_template,redirect,url_for,request
 app = Flask(__name__)
 
 
-@app.route("/",method=["GET","POST"])
+@app.route("/",methods=["GET","POST"])
 def login():
     # if request.method == "POST":
     #     #check user and password
@@ -12,12 +12,12 @@ def login():
     # else:
     return render_template("login.html")
 
-@app.route("/registration",method=["GET","POST"])
+@app.route("/registration",methods=["GET","POST"])
 def registration():
     # if request.method == "POST":
     #     #register user credentials
     # else:
-    return render_template("registration.html")
+    return render_template("register.html")
 
 # @app.route("/home")
 
