dfe1b8a4fdf85a06eecbed448ec49fc507566644
ZE JIE <ze-jie-kong-@hotmail.com>
2022-02-26 17:02:47 +0800

    added sessions and logout

--- __init__.py
+++ __init__.py
@@ -1,16 +1,23 @@
-from flask import Flask,render_template,redirect,url_for,request
+from flask import Flask,render_template,redirect,url_for,request,session
 
 app = Flask(__name__)
 
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route("/",methods=["GET","POST"])
 def login():
-    # if request.method == "POST":
-    #     #check user and password
-    #     # if request.form["user"] == "student":
-        
+    if request.method == "POST":
+        error = "User not found"
+        #check user and password
+        if request.form["uname"] in : #check in database
+            error = "Incorrect Password"
+            if request.form["psw"] in : #check in database
+                session['username'] = request.form["uname"]
+                return redirect(url_for('home'))
+        return render_template("login.html",error=error)
     # else:
-    return render_template("login.html")
+    return render_template("login.html",error="None")
 
 @app.route("/registration",methods=["GET","POST"])
 def registration():
@@ -19,7 +26,14 @@ def registration():
     # else:
     return render_template("register.html")
 
-# @app.route("/home")
+@app.route("/home")
+def home():
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run()   
